58c874ef1786684361b6b2381658ef5874c32d4e
evgenijaZ <evg.zubrich@gmail.com>
2017-10-01 22:01:21 +0300

    added Main

---
+++ lab1/src/com/lab111/labwork2/Main.java
@@ -0,0 +1,19 @@
+package com.lab111.labwork2;
+
+public class Main {
+
+	public Main(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args){
+
+	}
+}//end Main
