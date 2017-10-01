f03c5fff533484bbaeccac3d81e598e51b9b59bd
evgenijaZ <evg.zubrich@gmail.com>
2017-10-01 22:02:10 +0300

    added Interface1, Interface2, Interface3

---
+++ lab1/src/com/lab111/labwork2/Interface2.java
@@ -0,0 +1,13 @@
+package com.lab111.labwork2;
+
+public interface Interface2 {
+
+	public Class1 m_Class1 = new Class1();
+
+	public void method1();
+
+	public void method2();
+
+	public void method3();
+
+}
---
+++ lab1/src/com/lab111/labwork2/Interface3.java
@@ -0,0 +1,11 @@
+package com.lab111.labwork2;
+
+public interface Interface3 extends Interface2 {
+
+	public void method1();
+
+	public void method2();
+
+	public void method3();
+
+}
