a5265d53709ac90df0ea34b83d1acc1fd2c5a1cc
evgenijaZ <evg.zubrich@gmail.com>
2017-10-01 21:59:52 +0300

    added Class1

---
+++ lab1/src/com/lab111/labwork2/Class1.java
@@ -0,0 +1,23 @@
+package com.lab111.labwork2;
+
+public class Class1 extends Class3 implements Interface1 {
+
+	Class1(){
+
+	}
+
+	@Override
+	public void method1(){
+        System.out.println("Class1; Method1");
+    }
+
+	@Override
+	public void method2(){
+        System.out.println("Class1; Method2");
+	}
+
+	@Override
+	public void method3(){
+        System.out.println("Class1; Method3");
+	}
+}//end Class1
