e84698067d378bb2596748583c1ff926e9af4e20
evgenijaZ <evg.zubrich@gmail.com>
2017-10-04 21:00:57 +0300

    commented few lines

--- lab1/src/com/lab111/labwork2/Class2.java
+++ lab1/src/com/lab111/labwork2/Class2.java
@@ -5,18 +5,18 @@ public class Class2 implements Interface2 {
     public Class2() {
     }
 
-    @Override
-    public void method1() {
-        System.out.println("Class2; Method1");
-    }
+//    @Override
+//    public void method1() {
+//        System.out.println("Class2; Method1");
+//    }
 
     @Override
     public void method2() {
         System.out.println("Class2; Method2");
     }
 
-    @Override
-    public void method3() {
-        System.out.println("Class2; Method3");
-    }
+//    @Override
+//    public void method3() {
+//        System.out.println("Class2; Method3");
+//    }
 }//end Class2
