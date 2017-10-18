680f933759684f2893aed196078f7ba0e5656a0e
evgenijaZ <evg.zubrich@gmail.com>
2017-10-18 23:57:46 +0300

    added initialization for instance of Class1

--- lab1/src/com/lab111/labwork2/Main.java
+++ lab1/src/com/lab111/labwork2/Main.java
@@ -5,6 +5,9 @@ package com.lab111.labwork2;
  */
 public class Main {
 	public static void main(String[] args){
+		Class1 cl1 = new Class1();
+		cl1.setInterface1(new Class1());
+		cl1.setInterface2(new Class2());
 
 	}
 }//end Main
