1a9bc1cac9d3b1cedad88a9ac79430de239db249
evgenijaZ <evg.zubrich@gmail.com>
2017-10-23 22:52:48 +0300

    added javadoc comments

--- lab3/src/com/lab111/labwork3/Catalogue.java
+++ lab3/src/com/lab111/labwork3/Catalogue.java
@@ -3,10 +3,19 @@ package com.lab111.labwork3;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Directory in file system; A catalogue may contain files and other directories
+ */
 public class Catalogue implements FileObject {
-
+    /**
+     * Files and directories contained in this directory
+     */
     private List<FileObject> fileObjects = new ArrayList <>();
 
+    /**
+     * Calculates the size of the directory
+     * @return size of the directory
+     */
     @Override
     public int size() {
         int totalSize = 0;
@@ -17,9 +26,18 @@ public class Catalogue implements FileObject {
         return totalSize;
     }
 
+    /**
+     * Adds new file or catalogue in this directory
+     * @param item new file object
+     */
     void add(FileObject item){
         fileObjects.add(item);
     }
+
+    /**
+     * Removes a file or catalogue from this directory
+     * @param item file object that have to be deleted
+     */
 
     public void remove (FileObject item){
         fileObjects.remove(item);
--- lab3/src/com/lab111/labwork3/File.java
+++ lab3/src/com/lab111/labwork3/File.java
@@ -1,13 +1,27 @@
 package com.lab111.labwork3;
 
+/**
+ * File in file system;
+ */
 public class File implements FileObject {
-
+    /**
+     * Size of the file
+     */
     private int size;
 
+    /**
+     * Constructor of the file;
+     * Initializes size of file
+     * @param size size of new file
+     */
     public File(int size) {
         this.size = size;
     }
 
+    /**
+     * Calculates the size of the file
+     * @return size of the file
+     */
     @Override
     public int size() {
         System.out.println("Size of file:" + size);
