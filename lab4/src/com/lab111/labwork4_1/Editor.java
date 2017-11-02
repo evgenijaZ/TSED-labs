b923c0d8adb7414f716cfd59df55b63af13a8e66
evgenijaZ <evg.zubrich@gmail.com>
2017-11-02 13:24:40 +0200

    added class Editor

---
+++ lab4/src/com/lab111/labwork4_1/Editor.java
@@ -0,0 +1,12 @@
+package com.lab111.labwork4_1;
+
+class Editor {
+    /**
+     * Draws a composition
+     *
+     * @param figure to drawing
+     */
+    void draw(Composition figure) {
+        figure.drawComposition();
+    }
+}
