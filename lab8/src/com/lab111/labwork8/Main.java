68e4f0d865dfa8439aa1465c8ebfe4edddbcebb2
evgenijaZ <evg.zubrich@gmail.com>
2017-11-06 21:21:03 +0200

    test expression calculator

--- lab8/src/com/lab111/labwork8/Main.java
+++ lab8/src/com/lab111/labwork8/Main.java
@@ -3,6 +3,7 @@ package com.lab111.labwork8;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	ExpressionCalculator calculator = new ExpressionCalculator(5,10);
+	calculator.calculatePoints(3,6);
     }
 }
