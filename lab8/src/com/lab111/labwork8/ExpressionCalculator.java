a943a15320d60f883f98402c4176a72cc21bf0f0
evgenijaZ <evg.zubrich@gmail.com>
2017-11-06 21:20:15 +0200

    added methods for generating, adding and calculating exceptions

--- lab8/src/com/lab111/labwork8/ExpressionCalculator.java
+++ lab8/src/com/lab111/labwork8/ExpressionCalculator.java
@@ -1,4 +1,44 @@
 package com.lab111.labwork8;
 
-public class ExpressionCalculator {
+import java.util.ArrayList;
+
+ class ExpressionCalculator {
+
+    private ArrayList <AlgebraicExpression> expressions;
+
+    ExpressionCalculator() {
+        expressions = new ArrayList <>();
+    }
+
+    ExpressionCalculator(int countOfExpressions, int countOfPoints ) {
+        expressions = new ArrayList <>();
+        calculatePoints(countOfExpressions, countOfPoints);
+    }
+
+    private void addRandomExpressions(int countOfExpressions) {
+        for (int i = 0; i < countOfExpressions; i++) {
+            double a = Math.random() * 5 - 2.5;
+            double b = Math.random() * 5 - 2.5;
+            double c = Math.random() * 5 - 2.5;
+            AlgebraicExpression expression = new AlgebraicExpression(a, b, c);
+            expression.print();
+            expressions.add(expression);
+        }
+    }
+
+    void calculatePoints(int countOfExpressions, int countOfPoints) {
+        ValueMap valueMap = ValueMap.getInstance();
+        addRandomExpressions(countOfExpressions);
+        for (AlgebraicExpression expression : expressions) {
+            for (int i = 0; i < countOfPoints; i++) {
+                double x = Math.random() * 10 - 5;
+                double y = expression.function(x);
+                valueMap.addValue(new Point(x, y));
+            }
+
+        }
+        valueMap.plot();
+    }
+
+
 }
