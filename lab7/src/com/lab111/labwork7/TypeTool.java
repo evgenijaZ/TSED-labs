1f61f28fc7b1e534fb0d984ffd7685a5c45a6d1e
evgenijaZ <evg.zubrich@gmail.com>
2017-11-23 00:58:11 +0200

    added constructor

--- lab7/src/com/lab111/labwork7/TypeTool.java
+++ lab7/src/com/lab111/labwork7/TypeTool.java
@@ -1,9 +1,15 @@
 package com.lab111.labwork7;
 
 public class TypeTool implements GraphicTool {
+    private String text;
+
+    public TypeTool(String text) {
+        this.text = text;
+    }
+
     @Override
     public void rightClick(int x, int y) {
-        System.out.println("Typed text at position " + x + " " + y);
+        System.out.println("Typed text \"" + text + "\" at position " + x + ";" + y);
     }
 
     @Override
@@ -13,6 +19,6 @@ public class TypeTool implements GraphicTool {
 
     @Override
     public void dragAndDrop(int startX, int startY, int endX, int endY) {
-        System.out.println("Typed text from position " + startX + " " + startY + " to " + endX + " " + endY);
+        System.out.println("Typed text from position " + startX + ";" + startY + " to " + endX + ";" + endY);
     }
 }
