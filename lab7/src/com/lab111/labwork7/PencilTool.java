2a236f3654584ce181d8a41ef6e0f76e0fc5372e
evgenijaZ <evg.zubrich@gmail.com>
2017-11-22 23:42:48 +0200

    added PencilTool

---
+++ lab7/src/com/lab111/labwork7/PencilTool.java
@@ -0,0 +1,18 @@
+package com.lab111.labwork7;
+
+public class PencilTool implements GraphicTool {
+    @Override
+    public void rightClick(int x, int y) {
+        System.out.println("Drawn dot at position " + x + " " + y);
+    }
+
+    @Override
+    public void leftClick(int x, int y) {
+        System.out.println("The menu for changing the parameters of the pencil is displayed.");
+    }
+
+    @Override
+    public void dragAndDrop(int startX, int startY, int endX, int endY) {
+        System.out.println("Drawn line from position " + startX + " " + startY + " to " + endX + " " + endY);
+    }
+}
