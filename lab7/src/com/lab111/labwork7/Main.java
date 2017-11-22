951f9e6b4ad9afc6f15b62028d18cb9f58a4bc2f
evgenijaZ <evg.zubrich@gmail.com>
2017-11-23 00:59:05 +0200

    tested classes in main

--- lab7/src/com/lab111/labwork7/Main.java
+++ lab7/src/com/lab111/labwork7/Main.java
@@ -1,8 +1,12 @@
 package com.lab111.labwork7;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+	    Workspace workspace = new Workspace();
+	    workspace.setPencilTool();
+	    workspace.rightClick(5,85);
+	    workspace.dragAndDrop(12,0,34,32);
+	    workspace.setEyedropperTool();
+	    workspace.rightClick(54, 67);
     }
 }
