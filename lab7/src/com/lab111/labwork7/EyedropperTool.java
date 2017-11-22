bc752ea0e351c4c0ae4e7a0e6bbc638da941317e
evgenijaZ <evg.zubrich@gmail.com>
2017-11-23 00:58:27 +0200

    some fixes

--- lab7/src/com/lab111/labwork7/EyedropperTool.java
+++ lab7/src/com/lab111/labwork7/EyedropperTool.java
@@ -10,7 +10,7 @@ public class EyedropperTool implements GraphicTool {
         int R = random.nextInt(256);
         int G = random.nextInt(256);
         int B = random.nextInt(256);
-        System.out.println("Color at position " + x + " " + y + "is (" + R + "," + G + "," + B + ")");
+        System.out.println("Color at position " + x + ";" + y + " is rgb(" + R + "," + G + "," + B + ")");
     }
 
     @Override
--- lab7/src/com/lab111/labwork7/PencilTool.java
+++ lab7/src/com/lab111/labwork7/PencilTool.java
@@ -3,7 +3,7 @@ package com.lab111.labwork7;
 public class PencilTool implements GraphicTool {
     @Override
     public void rightClick(int x, int y) {
-        System.out.println("Drawn dot at position " + x + " " + y);
+        System.out.println("Drawn dot at position " + x + ";" + y);
     }
 
     @Override
@@ -13,6 +13,6 @@ public class PencilTool implements GraphicTool {
 
     @Override
     public void dragAndDrop(int startX, int startY, int endX, int endY) {
-        System.out.println("Drawn line from position " + startX + " " + startY + " to " + endX + " " + endY);
+        System.out.println("Drawn line from position " + startX + ";" + startY + " to " + endX + ";" + endY);
     }
 }
