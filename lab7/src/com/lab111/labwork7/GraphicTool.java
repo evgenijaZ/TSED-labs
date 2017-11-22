c1dfe5e0c2e3bd44f8f5b9f904f5a352f1261633
evgenijaZ <evg.zubrich@gmail.com>
2017-11-22 23:43:26 +0200

    added methods rightClick() and dragAndDrop()

--- lab7/src/com/lab111/labwork7/GraphicTool.java
+++ lab7/src/com/lab111/labwork7/GraphicTool.java
@@ -2,4 +2,6 @@ package com.lab111.labwork7;
 
 public interface GraphicTool {
     void rightClick(int x, int y);
+    void leftClick(int x, int y);
+    void dragAndDrop (int startX, int startY, int endX, int endY);
 }
