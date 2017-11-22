0e0061c88fc32b26c0ef0f69401809b472b00fb5
evgenijaZ <evg.zubrich@gmail.com>
2017-11-22 17:17:23 +0200

    added Main

---
+++ lab5/src/com/lab111/labwork5_Mediator/Main.java
@@ -0,0 +1,41 @@
+package com.lab111.labwork5_Mediator;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       Mediator mediator = new ConcreteMediator();
+        Table binomialName = new Table("AnimalBinomialNames");
+        Table family = new Table("AnimalFamilies");
+
+        binomialName.addColumn("First Name");
+        binomialName.addColumn("Last Name");
+        binomialName.addColumn("idFamily");
+
+        family.addColumn("FamilyName");
+
+        family.setValue(mediator, "FamilyName", 0,"Leporidae");
+        family.setValue(mediator,"FamilyName", 5,"Canninas");
+        family.addPKColumn(mediator, "id");
+
+
+        binomialName.setValue(mediator,"Last Name", 0,"Cuniculus");
+        binomialName.setValue(mediator,"First Name", 0,"Oryctolagus");
+        binomialName.setFKValue(mediator,"idFamily",0, family, "id", 7);
+
+
+        binomialName.print();
+        family.print();
+
+        binomialName.setFKValue(mediator,"idFamily",0, family, "id", 5);
+
+        binomialName.print();
+        family.print();
+
+//        family.deletePKCell(mediator, "id", 5);
+//
+//        binomialName.print();
+//        family.print();
+
+    }
+}
