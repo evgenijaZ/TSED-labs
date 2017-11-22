08510d8c1c950ee9b0e4ff33e4e10a7744aba92d
evgenijaZ <evg.zubrich@gmail.com>
2017-11-22 17:14:06 +0200

    added realization for setValue() and Receive()

--- lab5/src/com/lab111/labwork5_Mediator/PrimaryKeyCell.java
+++ lab5/src/com/lab111/labwork5_Mediator/PrimaryKeyCell.java
@@ -4,13 +4,29 @@ public class PrimaryKeyCell extends Cell {
 
     public PrimaryKeyCell(Mediator mediator, String name, Object value) {
         super(mediator, name, value);
+        if (mediator!= null) mediator.addCell(this);
     }
 
-    public void send(String message){
-        mediator.Send(message,this);
+    public PrimaryKeyCell(Mediator mediator, Cell cell){
+        super(mediator, cell.getName(), cell.getValue());
+      //  mediator.addCell(this);
     }
 
-    public void notify(String message){
-
+    public void Send(Object value, Cell to) {
+        mediator.Send(value,this, to);
     }
+
+    public void Receive(Object value, Cell from){
+        if (this.value == value)
+            this.Send(value, from);
+        else
+            this.Send(null, from);
+    }
+
+
+    public void setValue(Object value){
+        this.Send(value, null);
+        this.value = value;
+    }
+
 }
