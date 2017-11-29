4dc1612d696193f7cebe1d26a3ba93ae49a12879
evgenijaZ <evg.zubrich@gmail.com>
2017-11-29 18:22:56 +0200

    added an attribute name, constructor

--- lab9/src/com/lab111/labwork9/Table.java
+++ lab9/src/com/lab111/labwork9/Table.java
@@ -3,14 +3,19 @@ package com.lab111.labwork9;
 import java.util.List;
 
 public class Table {
+    private String name;
     private PrimaryKeyField primaryKeyField = null;
-    private List<Field> fields = null;
+    private List <Field> fields = null;
+
+    public Table(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
         result.append(primaryKeyField.toString());
-        for (Field field: fields) {
+        for (Field field : fields) {
             result.append(field.toString());
         }
         return result.toString();
@@ -20,11 +25,11 @@ public class Table {
         this.primaryKeyField = primaryKeyField;
     }
 
-    public void addField(Field field){
+    public void addField(Field field) {
         fields.add(field);
     }
 
-    public void addForeignKeyField(ForeignKeyField foreignKeyField){
+    public void addForeignKeyField(ForeignKeyField foreignKeyField) {
         fields.add(foreignKeyField);
     }
 }
