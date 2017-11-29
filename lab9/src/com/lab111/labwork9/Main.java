5f874ec115c1924fe0de93097067d7327be30078
evgenijaZ <evg.zubrich@gmail.com>
2017-11-29 23:33:42 +0200

    tested some methods

--- lab9/src/com/lab111/labwork9/Main.java
+++ lab9/src/com/lab111/labwork9/Main.java
@@ -3,6 +3,8 @@ package com.lab111.labwork9;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Builder builder = new TableBuilder();
+        Loader loader = new Loader(builder);
+        loader.buildTable("file.txt");
     }
 }
