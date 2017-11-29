372be5f8c9d48e94e26ce381fac196381d5730e9
evgenijaZ <evg.zubrich@gmail.com>
2017-11-29 17:57:18 +0200

    added class ForeignKeyField

---
+++ lab9/src/com/lab111/labwork9/ForeignKeyField.java
@@ -0,0 +1,12 @@
+package com.lab111.labwork9;
+
+public class ForeignKeyField extends Field {
+    public ForeignKeyField(String name, Class type) {
+        super(name, type);
+    }
+
+    @Override
+    public String toString() {
+        return (name + "\t" + type.getSimpleName() + " FK\n");
+    }
+}
